04952d782bbaf54c7dbd99d4eb07ade2a44b8444
KBC12006023\kbc12006023 <ryusei-miyazato-20h@stu.kbc.ac.jp>
2021-10-26 12:15:27 +0900

    script の修正

--- C_Team_Roll a Ball/Assets/Script/ButtonFunction.cs
+++ C_Team_Roll a Ball/Assets/Script/ButtonFunction.cs
@@ -1,28 +1,36 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
-
-public class ButtonFunction : MonoBehaviour {
-
-	[SerializeField]
-	//　ポーズした時に表示するUIのプレハブ
-	private GameObject menuUIPrefab;
-	//　ポーズUIのインスタンス
-	private GameObject menuUIInstance;
-	// Update is called once per frame
-	void Update () {
-		//Spaceキーが押されたら
-		if (Input.GetKeyDown(KeyCode.JoystickButton7)) {
-			//メニューが出ていなかったら
-			if (menuUIInstance == null)  {
-				menuUIInstance = GameObject.Instantiate (menuUIPrefab) as GameObject;
-				//ゲーム画面を止める
-				Time.timeScale = 0f;
-			} else {
-				Destroy (menuUIInstance);
-				Time.timeScale = 1f;
-			}
-		}
-	}
+using UnityEngine.SceneManagement;
+public class ButtonFunction : MonoBehaviour
+{
+    [SerializeField]
+    //　ポーズした時に表示するUIのプレハブ
+    private GameObject menuUIPrefab;
+    //　ポーズUIのインスタンス
+    private GameObject menuUIInstance;
+    // Update is called once per frame
+    void Update()
+    {
+        //Spaceキーが押されたら
+        if (Input.GetKeyDown(KeyCode.JoystickButton7))
+        {
+            //メニューが出ていなかったら
+            if (menuUIInstance == null)
+            {
+                menuUIInstance = GameObject.Instantiate(menuUIPrefab) as GameObject;
+                //ゲーム画面を止める
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                Destroy(menuUIInstance);
+                Time.timeScale = 1f;
+            }
+        }
+        else if (Time.timeScale == 0 && menuUIInstance == null)
+        {
+            Time.timeScale = 1f;
+        }
+    }
 }
