c6871ed88f2987cd0523e7e1c0e73829792c3d51
KBC12006023\kbc12006023 <ryusei-miyazato-20h@stu.kbc.ac.jp>
2021-11-30 14:08:57 +0900

    no message

--- C_Team_Roll a Ball/Assets/Script/SerectSound.cs
+++ C_Team_Roll a Ball/Assets/Script/SerectSound.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class SerectSound : MonoBehaviour
 {
     public bool DontDestroyEnabled = true;
@@ -12,8 +11,16 @@ public class SerectSound : MonoBehaviour
     // 使用する AudioClip をアタッチ
     [SerializeField] public AudioClip Cursor;
 
+    GameObject Pc, Bt;
+    PlayerController pc;
+    ButtonFunction bt;
+
     private void Start()
     {
+        Pc = GameObject.Find("Ball");
+        pc = Pc.GetComponent<PlayerController>();
+        Bt = GameObject.Find("Main Camera");
+        bt = Bt.GetComponent<ButtonFunction>();
         if (DontDestroyEnabled)
         {
             // Sceneを遷移してもオブジェクトが消えないようにする
@@ -22,6 +29,14 @@ public class SerectSound : MonoBehaviour
     }
 
     void Update()
+    {
+        if (Time.deltaTime == 0 && (pc.retryUIInstance != null || bt.menuUIInstance != null))
+        {
+            SelectSound();
+        }
+    }
+
+    void SelectSound()
     {
         if (Input.GetKeyDown(KeyCode.JoystickButton1) || Input.GetKeyDown(KeyCode.JoystickButton2))
         {
