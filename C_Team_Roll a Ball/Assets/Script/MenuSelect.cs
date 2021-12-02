175dff2e5b69a71ce99a81d5b5530742a73a1a4e
KBC12006023\kbc12006023 <ryusei-miyazato-20h@stu.kbc.ac.jp>
2021-12-02 10:13:33 +0900

    バージョン：build2-4c_1201_1631の修正(ゲーム終了する際にも音がなるように修正)

--- C_Team_Roll a Ball/Assets/Script/MenuSelect.cs
+++ C_Team_Roll a Ball/Assets/Script/MenuSelect.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MenuSelect : MonoBehaviour
 {
-    int number = 0;
+    public int number = 0;
     private float Trigger;
 
     // 使用する AudioSource をアタッチ
@@ -17,20 +17,96 @@ public class MenuSelect : MonoBehaviour
 
     public bool DontDestroyEnabled = true;
     public bool audioflag = false;
+    public bool sceneflag = false;
+
+    GameObject sound;
+    SerectSound serectsound;
 
     void Quit()
     {
-        if (audioflag)
-        {
-        }
-        else
-        {
 #if UNITY_EDITOR
-            UnityEditor.EditorApplication.isPlaying = false;
+        UnityEditor.EditorApplication.isPlaying = false;
 #elif UNITY_STANDALONE
       UnityEngine.Application.Quit();
 #endif
+    }
+    void SelectSound()
+    {
+        audioSource.PlayOneShot(Cursor2, 0.1f); //音を鳴らす
+        audioflag = true;
+    }
+    public void SceneSelect()
+    {
+        /*音が鳴り終わってからシーンを切り替えるように改造↓*/
+        if (SceneManager.GetActiveScene().name == "Stage0")
+        {
+            switch (number)
+            {
+                case 0:
+                    SceneManager.LoadScene("Stage0");
+                    break;
+                case 1:
+                    SceneManager.LoadScene("Menu");
+                    break;
+                case 2:
+                    Quit();
+                    break;
+            }
         }
+        if (SceneManager.GetActiveScene().name == "Stage1")
+        {
+            switch (number)
+            {
+                case 0:
+                    SceneManager.LoadScene("Stage1");
+                    break;
+                case 1:
+                    SceneManager.LoadScene("Menu");
+                    break;
+                case 2:
+                    Quit();
+                    break;
+            }
+        }
+        if (SceneManager.GetActiveScene().name == "Stage2")
+        {
+            switch (number)
+            {
+                case 0:
+                    SceneManager.LoadScene("Stage2");
+                    break;
+                case 1:
+                    SceneManager.LoadScene("Menu");
+                    break;
+                case 2:
+                    Quit();
+                    break;
+            }
+        }
+        if (SceneManager.GetActiveScene().name == "Stage3")
+        {
+            switch (number)
+            {
+                case 0:
+                    SceneManager.LoadScene("Stage3");
+                    break;
+                case 1:
+                    SceneManager.LoadScene("Menu");
+                    break;
+                case 2:
+                    Quit();
+                    break;
+            }
+        }
+        /*音が鳴り終わってからシーンを切り替えるように改造↑*/
+    }
+
+    void Start()
+    {
+        sound = GameObject.Find("SelectSound");
+        serectsound = sound.GetComponent<SerectSound>();
+        audioflag = false;
+        sceneflag = false;
     }
 
     void Update()
@@ -41,24 +117,28 @@ public class MenuSelect : MonoBehaviour
         // 座標を取得
         Vector3 pos = myTransform.position;
 
-        if (0 < Input.GetAxisRaw("Vertical") && Trigger == 0.0f)
+        if (0 < Input.GetAxisRaw("Vertical") && Trigger == 0.0f && sceneflag != true)
         {
             //音を鳴らす
             audioSource.PlayOneShot(Cursor1, 0.2f);
+
             number--;
             pos.y += 45;
+
             if (number < 0)
             {
                 number = 2;
                 pos.y -= 3 * 45;
             }
         }
-        if (0 > Input.GetAxisRaw("Vertical") && Trigger == 0.0f)
+        if (0 > Input.GetAxisRaw("Vertical") && Trigger == 0.0f && sceneflag != true)
         {
             //音を鳴らす
             audioSource.PlayOneShot(Cursor1, 0.2f);
+
             number++;
             pos.y -= 45;
+
             if (number > 2)
             {
                 number = 0;
@@ -69,80 +149,22 @@ public class MenuSelect : MonoBehaviour
         myTransform.position = pos;  // 座標を設定
         Trigger = Input.GetAxisRaw("Vertical"); //カーソルの移動速度制御
 
-        if (Input.GetKeyDown(KeyCode.JoystickButton1) || Input.GetKeyDown(KeyCode.JoystickButton2))
+        if (serectsound.soundflag)
         {
-            audioflag = true;
-            if (audioflag)
+            if (serectsound.audioSource.isPlaying)
             {
-                if (audioSource.isPlaying)
-                {
-                    audioflag = true;
-                }
-                else
-                {
-                    audioSource.PlayOneShot(Cursor2, 0.2f); //音を鳴らす
-                    audioflag = false;
-                }
+                sceneflag = true;
             }
-            if (SceneManager.GetActiveScene().name == "Stage0")
+        }
+        else
+        {
+            if (sceneflag)
             {
-                switch (number)
-                {
-                    case 0:
-                        SceneManager.LoadScene("Stage0");
-                        break;
-                    case 1:
-                        SceneManager.LoadScene("Menu");
-                        break;
-                    case 2:
-                        Quit();
-                        break;
-                }
+                SceneSelect();
             }
-            if (SceneManager.GetActiveScene().name == "Stage1")
+            else
             {
-                switch (number)
-                {
-                    case 0:
-                        SceneManager.LoadScene("Stage1");
-                        break;
-                    case 1:
-                        SceneManager.LoadScene("Menu");
-                        break;
-                    case 2:
-                        Quit();
-                        break;
-                }
-            }
-            if (SceneManager.GetActiveScene().name == "Stage2")
-            {
-                switch (number)
-                {
-                    case 0:
-                        SceneManager.LoadScene("Stage2");
-                        break;
-                    case 1:
-                        SceneManager.LoadScene("Menu");
-                        break;
-                    case 2:
-                        Quit();
-                        break;
-                }
-            }
-            if (SceneManager.GetActiveScene().name == "Stage3")
-            {
-                switch (number)
-                {
-                    case 0:
-                        SceneManager.LoadScene("Stage3");
-                        break;
-                    case 1:
-                        SceneManager.LoadScene("Menu");
-                        break;
-                    case 2:
-                        Quit();
-                        break;
-                }
+                sceneflag = false;
             }
         }
     }
