4ba32679f85ba364ec3bc7fe24eef98d33beefab
KBC12006023\kbc12006023 <ryusei-miyazato-20h@stu.kbc.ac.jp>
2021-12-09 15:01:31 +0900

    no message

---
+++ C_Team_Roll a Ball/Assets/Script/Particlesand.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particlesand : MonoBehaviour
+{
+    [SerializeField] ParticleSystem particle;
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            Debug.Log("触れている");
+            particle.Play(true);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            Debug.Log("離れている");
+            particle.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+        }
+    }
+}
