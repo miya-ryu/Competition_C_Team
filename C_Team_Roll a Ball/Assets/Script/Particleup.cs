bc01cdbca63f9be12c9ce0d4627731faadc6d114
KBC12006023\kbc12006023 <ryusei-miyazato-20h@stu.kbc.ac.jp>
2021-12-09 10:11:43 +0900

    no message

---
+++ C_Team_Roll a Ball/Assets/Script/Particleup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particleup : MonoBehaviour
+{
+    private bool isPlaying;
+    [SerializeField] ParticleSystem particle;
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            if (isPlaying)
+            {
+                Debug.Log("触れている");
+                particle.Play(true);
+            }
+        }
+        else
+        {
+            particle.Stop(false, ParticleSystemStopBehavior.StopEmitting);
+        }
+        isPlaying = !isPlaying;
+    }
+}
